96d444fa551e9e62992e3d4b422f544d45ebe7e2
Tony Faieta <tfaieta@gmail.com>
2018-07-01 19:28:51 -0400

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+# Hi and Welcome
+This is what makes up [tfaieta.com](tfaieta.com)
 
+## Installation
+If you want to run this site locally:
+
+- `git clone`
+- Install the [Gatsby](https://www.gatsbyjs.org/) CLI: `npm i -g gatsby-cli`
+- Run `npm install`
+- Run `gatsby develop`
+
+To ship it out run `gatsby build` first.
+
+Feel free to use it if you want as a template for your site. I will be writing a tutorial on how to use Firebase to host
+a site like this for free, once that is up I'll post it here. 
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,12 +14,12 @@ const IndexPage = () => {
           />
           <div className="h1">
           <div className="mega sans mxl">
-Hi, I'm Tony. I like to design and build software that break down barriers for people to create and help others.
+Hi, I'm Tony. I like to design and build software that helps people create interesting and helpful content.
           </div>
             {mdToHTML(`
 Right now I am working on my startup called [Tess](https://tess.fm/), we're an all-in-one podcast solution for creators and we also made a really cool
-player for listeners. I'm also an undergrad CS student at the University of Florida. I like to have conversations with others, so feel free
-to tweet at me [@tfaieta](https://twitter.com/tfaieta).
+player for listeners. I'm also an undergrad CS student at the University of Florida where I like to have conversations with others and write about the things that
+make me happy or about topics that are important to me. If you want to have a conversation feel free to tweet at me [@tfaieta](https://twitter.com/tfaieta).
     `)}
           </div>
       </div>)
