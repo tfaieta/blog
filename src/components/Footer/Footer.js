911c57fb9d20529df0969cdf4d16d287f2e28da1
Tony Faieta <tfaieta@gmail.com>
2019-03-08 23:07:53 -0500

    Testing CI + changed npm dev script

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <Wrapper>
       <Right>
-        This site was made with <span role="img" aria-label="Heart">❤️</span> in Gainesville, FL (still in development)
+        This site was made with <span role="img" aria-label="Heart">❤️</span> in Gainesville, FL (this site is still a draft)
       </Right>
       {/* <AllMyContactInfo /> */}
     </Wrapper>
